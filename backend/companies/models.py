f2b3521edd04d9a03291ad22c5a3c7cd8e0eb3c8
Richard Matheus <richardmatheus929@gmail.com>
2024-05-24 10:11:35 -0300

    Criando o accounts

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Enterprise(models.Model):
     nome = models.CharField(max_length=150)
     user = models.ForeignKey("accounts.User",on_delete=models.CASCADE)
+
+class Employee(models.Model):
+    user = models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise,on_delete=models.CASCADE)
